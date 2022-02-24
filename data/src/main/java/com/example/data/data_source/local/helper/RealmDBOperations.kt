48f784a2f8a803f40bd7f6b03cbd0859c2300a36
emile.wakim <emile.wakim96@gmail.com>
2022-02-24 12:45:39 +0100

    use dispatcher instance from hilt

--- data/src/main/java/com/example/data/data_source/local/helper/RealmDBOperations.kt
+++ data/src/main/java/com/example/data/data_source/local/helper/RealmDBOperations.kt
@@ -2,19 +2,20 @@ package com.example.data.data_source.local.helper
 
 import com.example.data.data_source.local.entity.TreeRealm
 import com.example.data.data_source.remote.mappers.mapToTree
+import com.example.data.data_source.util.DispatcherProvider
 import com.example.domain.models.Tree
 import io.realm.Realm
 import io.realm.kotlin.executeTransactionAwait
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 
 class RealmDBOperations @Inject constructor(
-    private var realm: Realm
+    private var realm: Realm,
+    private val dispatcher: DispatcherProvider
 ) {
 
     suspend fun insertTree(treeRealm: TreeRealm) {
-        realm.executeTransactionAwait(Dispatchers.IO) { realmTransaction ->
+        realm.executeTransactionAwait(dispatcher.io) { realmTransaction ->
             realmTransaction.insertOrUpdate(treeRealm)
         }
     }
@@ -22,7 +23,7 @@ class RealmDBOperations @Inject constructor(
     suspend fun getTrees(): List<Tree> {
         val trees = mutableListOf<Tree>()
 
-        realm.executeTransactionAwait(Dispatchers.IO) { realmTransaction ->
+        realm.executeTransactionAwait(dispatcher.io) { realmTransaction ->
             trees.addAll(realmTransaction
                 .where(TreeRealm::class.java)
                 .findAll()
