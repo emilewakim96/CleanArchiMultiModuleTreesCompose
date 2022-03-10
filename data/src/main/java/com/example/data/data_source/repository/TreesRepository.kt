6f7004801c8fde89e2ea96c84e56eb178b02a852
emile.wakim <emile.wakim96@gmail.com>
2022-03-10 15:45:18 +0100

    clean code

--- data/src/main/java/com/example/data/data_source/repository/TreesRepository.kt
+++ data/src/main/java/com/example/data/data_source/repository/TreesRepository.kt
@@ -4,8 +4,10 @@ import com.example.data.data_source.remote.responses.Tree
 
 interface TreesRepository {
 
+    /* api */
     suspend fun getTreesList(forceRefresh: Boolean): List<Tree>?
 
+    /* room */
     suspend fun getTreesListFromDB(): List<Tree>?
 
     suspend fun saveTreeInDB(tree: Tree)
